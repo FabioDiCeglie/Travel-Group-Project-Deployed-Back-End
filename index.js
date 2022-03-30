74f15832dd174c76aba9729f46913b2f613c76b4
FabioDiCeglie <dcfabio96@gmail.com>
2022-03-30 10:14:21 +0200

    change for deploy on Heroku

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const corsMiddleWare = require("cors");
+const cors = require("cors");
 // Auth middleware: our own code. Checks for the existence of a token in a header called `authentication`.
 const authMiddleWare = require("./auth/middleware");
 const authRouter = require("./routers/auth");
@@ -10,19 +10,7 @@ const { PORT } = require("./config/constants");
 
 // Create an express app
 const app = express();
-
-/**
- * Middlewares
- *
- * It is advisable to configure your middleware before configuring the routes
- * If you configure routes before the middleware, these routes will not use them
- *
- */
-
-// CORS middleware:  * Since our api is hosted on a different domain than our client
-// we are are doing "Cross Origin Resource Sharing" (cors)
-// Cross origin resource sharing is disabled by express by default
-app.use(corsMiddleWare());
+app.use(cors());
 
 // express.json():be able to read request bodies of JSON requests a.k.a. body-parser
 const bodyParserMiddleWare = express.json();
